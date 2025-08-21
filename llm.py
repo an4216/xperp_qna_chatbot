7cfb5a198a81669be803f71b7d53980ec1f7dc9f
김성규 <rlatjdrb7933@gmail.com>
2025-08-21 15:34:09 +0900

    주소변경

--- llm.py
+++ llm.py
@@ -27,7 +27,7 @@ import re
 # =========================================
 # 환경설정
 # =========================================
-OLLAMA_HOST = os.getenv("OLLAMA_HOST", "https://5c07cbtddeey10-11434.proxy.runpod.net")
+OLLAMA_HOST = os.getenv("OLLAMA_HOST", "https://olummmzfsw4k5k-11434.proxy.runpod.net")
 MODEL_LLM   = os.getenv("MODEL_LLM", "gemma3:27b")  # ollama pull gemma3:latest
 TOP_K       = int(os.getenv("TOP_K", "4"))
 VECTOR_DIR  = os.getenv("VECTOR_DIR", "vectorstore")
